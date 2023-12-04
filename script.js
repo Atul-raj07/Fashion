5b15ace762243b7b46302faa757908a7315b5438
Atul-raj07 <atulrajgupta12j@gmail.com>
2023-12-04 22:54:38 +0530

    testing

---
+++ script.js
@@ -0,0 +1,13 @@
+var a = +prompt ("no.please")
+
+if (a === 100){
+    console.log(a)
+
+}
+else if (a != 100){
+ var b = +prompt ("2no.please")
+ console.log(a+b)
+}
+else{
+    console.error("galat hai be")
+}
